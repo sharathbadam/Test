b3715b4c4d4d6e730430d56162834dccc06a26c5
@abdam78683! <a-badam@wiu.edu>
2023-06-03 14:42:25 -0500

    new example

---
+++ com/examples/Firstclass.java
@@ -0,0 +1,14 @@
+package com.examples;
+
+import com.examples.OOPS.Vehicle;
+
+public class Firstclass {
+
+    public static void main(String[] args) {
+        Vehicle vehicle= new Vehicle();
+
+       vehicle.Start();
+        vehicle.Stop();
+    }
+
+}
---
+++ com/examples/OOPS/Vehicle.java
@@ -0,0 +1,11 @@
+package com.examples.OOPS;
+
+public class Vehicle {
+    public void Start() {
+        System.out.println("Start the Vehicle");
+    }
+    public void Stop() {
+        System.out.println("Stop the Vehicle");
+    }
+
+}
